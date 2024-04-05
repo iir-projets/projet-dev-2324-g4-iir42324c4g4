fe20a2724b349a31eee2e86c89be9ca8c2e69334
asmae <asmaeabouhadid4.4@gmail.com>
2024-04-05 01:43:26 +0000

    Premier commit

---
+++ src/test/java/com/example/gestcinema/GestCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestcinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
